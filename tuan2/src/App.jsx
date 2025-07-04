93e8adf7cd3bc027cb03377d7c9b1f8593683e79
toankunsss <nguyenvantoanmnsh@gmail.com>
2025-07-04 18:36:16 +0700

    fix app file

--- tuan2/src/App.jsx
+++ tuan2/src/App.jsx
@@ -1,7 +1,26 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout'
+import Dashboard from '../pages/Dashboard'
+import MyTasks from '../pages/MyTasks'
+import NotFound from '../pages/NotFound'
+import Setting from '../pages/Setting'
+import VitalTask from '../pages/VitalTask'
+import TaskCategory from '../pages/TaskCategory'
+
+const App = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<MainLayout/>}>
+                <Route index element={<Dashboard/>}/>
+                <Route path='task' element={<MyTasks/>}/>
+                <Route path='vitalTask' element={<VitalTask/>}/>
+                <Route path='taskcategory' element={<TaskCategory/>}/>
+                <Route path='setting' element={<Setting/>}/>
+            </Route>
+            <Route path='*' element={<NotFound/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
--- tuan2/src/main.jsx
+++ tuan2/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Approuter from './routers/Approuter.jsx'
 import App from './App.jsx'
 import 'antd/dist/reset.css'
 import './styles/global.scss'
@@ -9,6 +8,6 @@ import './styles/_variables.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Approuter/>
+    <App/>
   </StrictMode>
 )
